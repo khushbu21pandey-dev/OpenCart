9932f8b375e1ef03eee36a88ebd228d07cef8cec
Khushbu <khushbu23pandey@gmail.com>
2025-10-25 19:38:55 +0530

    committedd 2nd time

--- src/test/java/Opencart/cartt/OpenCartStandAloneTest.java
+++ src/test/java/Opencart/cartt/OpenCartStandAloneTest.java
@@ -3,11 +3,13 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 public class OpenCartStandAloneTest {
 public static void main(String[] args)
@@ -39,6 +41,44 @@ public static void main(String[] args)
 	}
 	
  }
+ WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(10));
+ WebElement cartLink = wait2.until(ExpectedConditions.elementToBeClickable(
+         By.partialLinkText("Shopping Cart")));
+ ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", cartLink);
+ cartLink.click();
+ System.out.println("Navigated to Shopping Cart page successfully");
+ WebDriverWait wait3 = new WebDriverWait(driver, Duration.ofSeconds(10));
+//code for checkout
+ WebElement chkout= wait3.until(ExpectedConditions.elementToBeClickable(By.linkText("Checkout")));
+ chkout.click();
+//code to fill the chekout form
+ WebDriverWait wait4 = new WebDriverWait(driver, Duration.ofSeconds(10));
+ WebElement bill= wait4.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[normalize-space()='Step 2: Billing Details']")));
+  bill.click();
+ System.out.println("clicked on billing details");
+ driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+ try {
+	    WebElement newAddr = driver.findElement(By.xpath("//input[@name='payment_address' and @value='new']"));
+	    if (newAddr.isDisplayed()) newAddr.click();
+	} catch (Exception e) {
+	    System.out.println("Using existing address.");
+	}
+ driver.findElement(By.id("input-payment-firstname")).sendKeys("Khushbu");
+ driver.findElement(By.id("input-payment-lastname")).sendKeys("Pandey");
+ driver.findElement(By.id("input-payment-address-1")).sendKeys("Gomti Nagar");
+ driver.findElement(By.id("input-payment-city")).sendKeys("Lucknow");
+ driver.findElement(By.id("input-payment-postcode")).sendKeys("226010");
+ Select cntry= new Select(driver.findElement(By.id("input-payment-country")));
+ cntry.selectByVisibleText("India");
+ Select rgn= new Select(driver.findElement(By.id("input-payment-zone")));
+ rgn.selectByVisibleText("Bihar");
+ driver.findElement(By.id("button-payment-address")).click();
+ System.out.println("clicked on the continue button");
+ System.out.println("clicked on the continue button");
+ driver.quit();
+ 
+ 
+ 
  
  
   
