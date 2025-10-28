18a653e8f11310ccfb0d48456201e1b0629403ab
Khushbu <khushbu23pandey@gmail.com>
2025-10-28 21:06:49 +0530

    My 4th commit

--- src/test/java/Opencart/cartt/OpenCartStandAloneTest.java
+++ src/test/java/Opencart/cartt/OpenCartStandAloneTest.java
@@ -11,8 +11,10 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 public class OpenCartStandAloneTest {
-public static void main(String[] args)
+	@Test
+public void standAlonetest()
 {
 	String prodName="MacBook";
   WebDriver driver=new ChromeDriver();
--- src/test/java/Opencart/cartt/OpenCartStandTest.java
+++ src/test/java/Opencart/cartt/OpenCartStandTest.java
@@ -1,11 +1,13 @@
 package Opencart.cartt;
 import java.time.Duration;
 
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenCartStandTest {
-public static void main(String[] args)
+	@Test
+public  void cartStandTest()
 {
 	WebDriver driver=new ChromeDriver()	;
 	driver.manage().window().maximize();
